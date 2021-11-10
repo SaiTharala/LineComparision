d3080b66df2da6e394e9c7048d9ce160198a997f
unknown <Saitharala@gmail.com>
2021-11-10 21:54:37 +0530

    [Tharala SaiPrashanth] Added EqualityOfTwoLines Feature

--- LineComparisionProgram/LineComparisionProgram/LineComparision.cs
+++ LineComparisionProgram/LineComparisionProgram/LineComparision.cs
@@ -13,10 +13,17 @@ namespace LineComparisonComputation
             this.yOnePt = yOnePt;
             this.yTwoPt = yTwoPt;
         }
-        public double CalculatingLength()
+        public double LengthCalc()
         {
             Console.WriteLine(Math.Sqrt(Math.Pow(this.xTwoPt - this.xOnePt, 2)) + Math.Sqrt(Math.Pow(this.yTwoPt - this.yOnePt, 2)));
             return Math.Sqrt(Math.Pow(this.xTwoPt - this.xOnePt, 2)) + Math.Sqrt(Math.Pow(this.yTwoPt - this.yOnePt, 2));
+        }
+        public void Check(double lengthOne, double lengthTwo)
+        {
+            if (lengthOne.Equals(lengthTwo))
+                Console.WriteLine("Two lines are equal");
+            else
+                Console.WriteLine("Two lines are not equal");
         }
     }
 }
